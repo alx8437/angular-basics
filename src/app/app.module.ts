876dbe6bd6968a5eda04edd28e796ea08439c2d7
Alx8437 <alx8437@mail.ru>
2020-07-09 00:01:12 +0300

    add first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PostComponent} from "./Post/post.component";
+import {CounterComponent} from "./Counter/counter.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule
