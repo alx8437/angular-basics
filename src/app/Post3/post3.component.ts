45447cd6b7641848da6954f3df8ff189e296c8bd
Alx8437 <alx8437@mail.ru>
2020-07-09 17:35:42 +0300

    add component by ng g c and add component.ts

---
+++ src/app/Post3/post3.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-post3',
+  template: `
+    <div class="post3">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, sit.</p>
+    </div>
+  `,
+  styles: [`
+    .post3 {
+      height: 400px;
+      width: 300px;
+      background-color: orange;
+    }
+  `]
+})
+
+export class Post3Component {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {PostComponent} from "./Post/post.component";
-import {CounterComponent} from "./Counter/counter.component";
+import {PostComponent} from './Post/post.component';
+import {CounterComponent} from './Counter/counter.component';
+import { Post2Component } from './post2/post2.component';
+import {Post3Component} from './Post3/post3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    CounterComponent
+    CounterComponent,
+    Post2Component,
+    Post3Component
   ],
   imports: [
     BrowserModule
